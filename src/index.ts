a9b84056f68b8793ebce315e43a977b97816e664
Chigozie Jacob <45979359+Chigozie0706@users.noreply.github.com>
2023-07-07 15:54:54 +0000

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,62 @@
 import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
-type Message = Record<{
+type Event = Record<{
     id: string;
-    title: string;
-    body: string;
-    attachmentURL: string;
+    eventTitle: string;
+    eventDescription: string;
+    eventCardImgUrl: string;
+    eventDate: string;
     createdAt: nat64;
     updatedAt: Opt<nat64>;
 }>
 
-type MessagePayload = Record<{
-    title: string;
-    body: string;
-    attachmentURL: string;
+type EventPayload = Record<{
+    eventTitle: string;
+    eventDescription: string;
+    eventCardImgUrl: string;
+    eventDate: string;
 }>
 
-const messageStorage = new StableBTreeMap<string, Message>(0, 44, 1024);
+const eventStorage = new StableBTreeMap<string, Event>(0, 44, 1024);
 
 $query;
-export function getMessages(): Result<Vec<Message>, string> {
-    return Result.Ok(messageStorage.values());
+export function getAllEvents(): Result<Vec<Event>, string> {
+    return Result.Ok(eventStorage.values());
 }
 
 $query;
-export function getMessage(id: string): Result<Message, string> {
-    return match(messageStorage.get(id), {
-        Some: (message) => Result.Ok<Message, string>(message),
-        None: () => Result.Err<Message, string>(`a message with id=${id} not found`)
+export function getEventById(id: string): Result<Event, string> {
+    return match(eventStorage.get(id), {
+        Some: (event) => Result.Ok<Event, string>(event),
+        None: () => Result.Err<Event, string>(`a message with id=${id} not found`)
     });
 }
 
 $update;
-export function addMessage(payload: MessagePayload): Result<Message, string> {
-    const message: Message = { id: uuidv4(), createdAt: ic.time(), updatedAt: Opt.None, ...payload };
-    messageStorage.insert(message.id, message);
-    return Result.Ok(message);
+export function addMessage(payload: EventPayload): Result<Event, string> {
+    const event: Event = { id: uuidv4(), createdAt: ic.time(), updatedAt: Opt.None, ...payload };
+    eventStorage.insert(event.id, event);
+    return Result.Ok(event);
 }
 
 $update;
-export function updateMessage(id: string, payload: MessagePayload): Result<Message, string> {
-    return match(messageStorage.get(id), {
-        Some: (message) => {
-            const updatedMessage: Message = {...message, ...payload, updatedAt: Opt.Some(ic.time())};
-            messageStorage.insert(message.id, updatedMessage);
-            return Result.Ok<Message, string>(updatedMessage);
+export function updateEvent(id: string, payload: EventPayload): Result<Event, string> {
+    return match(eventStorage.get(id), {
+        Some: (event) => {
+            const updatedEvent: Event = {...event, ...payload, updatedAt: Opt.Some(ic.time())};
+            eventStorage.insert(event.id, updatedEvent);
+            return Result.Ok<Event, string>(updatedEvent);
         },
-        None: () => Result.Err<Message, string>(`couldn't update a message with id=${id}. message not found`)
+        None: () => Result.Err<Event, string>(`couldn't update a message with id=${id}. message not found`)
     });
 }
 
 $update;
-export function deleteMessage(id: string): Result<Message, string> {
-    return match(messageStorage.remove(id), {
-        Some: (deletedMessage) => Result.Ok<Message, string>(deletedMessage),
-        None: () => Result.Err<Message, string>(`couldn't delete a message with id=${id}. message not found.`)
+export function deleteEvent(id: string): Result<Event, string> {
+    return match(eventStorage.remove(id), {
+        Some: (deletedEvent) => Result.Ok<Event, string>(deletedEvent),
+        None: () => Result.Err<Event, string>(`couldn't delete a message with id=${id}. message not found.`)
     });
 }
 
