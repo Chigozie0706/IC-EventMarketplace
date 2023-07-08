334fb3116f02a4bc7a4393a1b6411c4168c9d6f0
Chigozie Jacob <45979359+Chigozie0706@users.noreply.github.com>
2023-07-08 14:13:05 +0000

    updated code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
+import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt, Principal } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 type Event = Record<{
+    owner : Principal;
     id: string;
     eventTitle: string;
     eventDescription: string;
     eventCardImgUrl: string;
-    eventDate: string;
+    eventLocation: string;
+    attendees : Vec<string>;
     createdAt: nat64;
     updatedAt: Opt<nat64>;
 }>
@@ -16,6 +18,7 @@ type EventPayload = Record<{
     eventDescription: string;
     eventCardImgUrl: string;
     eventDate: string;
+    eventLocation : string;
 }>
 
 const eventStorage = new StableBTreeMap<string, Event>(0, 44, 1024);
@@ -34,8 +37,8 @@ export function getEventById(id: string): Result<Event, string> {
 }
 
 $update;
-export function addMessage(payload: EventPayload): Result<Event, string> {
-    const event: Event = { id: uuidv4(), createdAt: ic.time(), updatedAt: Opt.None, ...payload };
+export function addEvent(payload: EventPayload): Result<Event, string> {
+    const event: Event = { owner: ic.caller(), id: uuidv4(), attendees : [], createdAt: ic.time(), updatedAt: Opt.None, ...payload };
     eventStorage.insert(event.id, event);
     return Result.Ok(event);
 }
@@ -54,11 +57,51 @@ export function updateEvent(id: string, payload: EventPayload): Result<Event, st
 
 $update;
 export function deleteEvent(id: string): Result<Event, string> {
-    return match(eventStorage.remove(id), {
-        Some: (deletedEvent) => Result.Ok<Event, string>(deletedEvent),
+    return match(eventStorage.get(id), {
+        Some: (deletedEvent) => {
+        if(deletedEvent.owner.toString() !== ic.caller().toString()){
+            return Result.Err<Event, string>("You are not the owner of this event")
+        }
+        eventStorage.remove(id)
+        Result.Ok<Event, string>(deletedEvent)
+    },
         None: () => Result.Err<Event, string>(`couldn't delete a message with id=${id}. message not found.`)
     });
 }
+
+
+$update;
+export function attendAnEvent(userId: string): Result<Event, string> {
+    // Get the user profile requesting the follow
+    const event = match(eventStorage.get(userId), {
+        Some: (event) => {
+            // Check if the event is already following the account to be followed
+            // Return the event profile if account to be followed is already being followed
+            const caller = ic.caller().toString()
+            if(event.attendees.includes(caller)) {
+                return Result.Ok<Event, string>(event)
+            } else { // Else run the code below
+                // Save the user's initial following in a variable
+                const attendees: Vec<string> = event.attendees;
+                // Add the new user to be followed to the existing users already followed
+                attendees.push(caller);
+
+                const user1Profile: Event = {
+                    ...event,
+                    attendees: attendees // Assign the following variable to the list all of the users followed including the new user
+                }
+                // Save the current user's updated status in the userProfileStorage
+                eventStorage.insert(event.id, user1Profile);
+                // Return the user's profile with the updated changes
+                return Result.Ok<Event, string>(user1Profile);
+            }
+        },
+        None: () => Result.Err<Event, string>("Unable to carry out the following function")
+    })
+
+    return event
+}
+
 
 // a workaround to make uuid package work with Azle
 globalThis.crypto = {
